dd082c149e362bc0deb1fea1a80f9d30d4c4ef1d
laanspoon <TiaraRLanson@gmail.com>
2025-01-18 13:28:55 -0500

    added menu bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import StickyNote from "./components/stickyNote";
-import TextBox from "./components/addNote";
+import AddNote from "./components/addNote";
+import MenuBar from "./components/menuBar";
 
 export default function Home() {
   return (
     <div>
       <StickyNote/>
-      <TextBox/>
+      <AddNote/>
+      <MenuBar/>
     </div>
   );
 }
