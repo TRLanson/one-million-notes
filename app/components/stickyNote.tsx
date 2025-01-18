afc60832e3ea47012f94c6b976e9d9e67bfae9bb
Evan He <evan.he24@gmail.com>
2025-01-18 10:10:01 -0500

    sticky note

---
+++ app/components/stickyNote.tsx
@@ -0,0 +1,9 @@
+const StickyNote = () => {
+    return (
+        <div
+            className="bg-yellow-100 w-48 h-48 border-1 rounded-lg shadow-lg text-white">
+        </div>
+    );
+};
+
+export default StickyNote;
